ae25605d845c69a73c817918a0bef4b0005d3f95
Roney Freitas <roneyfreitas.rf@gmail.com>
2020-08-26 23:45:22 -0300

    Criado a Estrutura de Paginas

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Index = () => {
-  return <h1>Olá Mundo!!!</h1>
+  return (
+    <div>
+      <h1>Olá Mundo!!!</h1>
+      <div>
+        <Link href='/sobre' >
+          <a>Sobre</a>
+        </Link>
+        <Link href='/contato' >
+          <a>Contato</a>
+        </Link>
+        <Link href='/pesquisa' >
+          <a>Pesquisa</a>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Index
