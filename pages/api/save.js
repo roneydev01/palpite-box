f3fc4033edfc1fada3ed31aab9d07877c9eba9bc
Roney Freitas <roneyfreitas.rf@gmail.com>
2020-09-03 19:48:08 -0300

    Adicionando Data e Gerando Cupom

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,8 +1,17 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
+import moment from 'moment'
 import credentials from '../../credentials.json'
 
 //Id da Planllha
 const doc = new GoogleSpreadsheet('1o-eMKiKfNDCbdNtOJtKf2dDQr3OFLNB8jGrWNP1YMDM')
+
+//Gera Cupom
+const genCupom = () => {
+  //Gera cupom a partir da data e converte para hexadecimal
+  const code = parseInt(moment().format('YYMMDHHmmsSSS')).toString(16)
+  //Retorna com os traços
+  return code.substr(0, 4) + '-' + code.substr(4, 4) + '-' + code.substr(8, 4)
+}
 
 export default async (req, res) => {
   try {
@@ -12,12 +21,30 @@ export default async (req, res) => {
     //Pegando os dados da requisição
     const data = JSON.parse(req.body)
 
+    //Dados da Planilha 2
+    const sheetConfig = doc.sheetsByIndex[2]
+    await sheetConfig.loadCells('A4:B4')
+
+    const mostrarPromocalCell = sheetConfig.getCell(3, 0)
+    const textoCell = sheetConfig.getCell(3, 1)
+
+    let Cupom = ''
+    let Promo = ''
+
+    if (mostrarPromocalCell.value === 'VERDADEIRO') {
+      //Chama a Função genCupon
+      Cupom = genCupom()
+      Promo = textoCell.value
+    }
+
     await sheet.addRow({
       Nome: data.Nome,
       Email: data.Email,
       Whatsapp: data.Whatsapp,
-      Cupom: 'dweqw122',
-      Promo: 'dsdfdfsa'
+      Nota: 5,
+      'Data': moment().format('DD/MM/YYYY HH:mm:ss'),
+      Cupom,
+      Promo
     })
     //Enviado dados para a planilha
     res.end(req.body)
