1438ea8254c52fc3f71234cefadb19970b326b4b
Roney Freitas <roneyfreitas.rf@gmail.com>
2020-09-02 22:29:00 -0300

    Criando Save da API

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -2,6 +2,24 @@ import React from "react"
 import Link from "next/link"
 
 const Pesquisa = () => {
+  const save = async () => {
+    const form = {
+      Nome: 'Teste nome',
+      Email: 'Teste email',
+      Whatsapp: 'Teste wats'
+    }
+    try {
+      const response = await fetch('api/save', {
+        method: 'POST',
+        body: JSON.stringify(form)
+      })
+      const data = await response.json()
+      console.log(data)
+    } catch (err) {
+
+    }
+  }
+
   return (
     <div className='pt-6'>
       <h1 className='text-center font-bold my-4 text-2xl text-green-600'>Críticas e sugestões</h1>
@@ -11,6 +29,7 @@ const Pesquisa = () => {
       <div className='w-1/5 mx-auto'>
         <label className='font-bold'>Seu Nome</label>
         <input type='text' className='p-4 block shadow bg-green-200 my-2 rounded ' />
+        <button className='bg-green-500 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow' onClick={save}>Salvar</button>
       </div>
     </div>
   )
